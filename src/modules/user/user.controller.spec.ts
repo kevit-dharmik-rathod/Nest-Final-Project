3163378bafb5b0d57a4528723f5a370dffed56cf
Dharmik Kevit <dharmik.rathod@kevit.io>
2023-12-12 18:00:40 +0530

    testing start

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { RolesGuard } from 'src/guards/roles.guard';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -8,7 +9,7 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [UserService, RolesGuard],
     }).compile();
 
     controller = module.get<UserController>(UserController);
